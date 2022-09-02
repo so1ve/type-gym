3b766a1e9746ed591778d06afb579a340cde4739
so1ve <nn_201312@163.com>
2022-09-02 09:11:02 +0000

    feat: Exclusive

--- src/utils/enhancement.ts
+++ src/utils/enhancement.ts
@@ -7,3 +7,5 @@ export type DeepReadonly<T> = T extends object ? { readonly [K in keyof T]: Deep
 export type DeepWritable<T> = T extends object ? { -readonly [K in keyof T]: DeepWritable<T[K]> } : T;
 
 export type DeepNonNullable<T> = T extends object ? { [K in keyof T]: NonNullable<DeepNonNullable<T[K]>> } : T;
+
+export type Exclusive<T, U> = Exclude<T | U, T & U>;
