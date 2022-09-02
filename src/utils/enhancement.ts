c20385540ae1f7fa0334dd2ede903bf2ccc7332d
so1ve <nn_201312@163.com>
2022-09-02 09:04:29 +0000

    feat: DeepNonNullable

--- src/utils/enhancement.ts
+++ src/utils/enhancement.ts
@@ -5,3 +5,5 @@ export type DeepRequired<T> = T extends object ? { [K in keyof T]-?: DeepRequire
 export type DeepReadonly<T> = T extends object ? { readonly [K in keyof T]: DeepReadonly<T[K]> } : T;
 
 export type DeepWritable<T> = T extends object ? { -readonly [K in keyof T]: DeepWritable<T[K]> } : T;
+
+export type DeepNonNullable<T> = T extends object ? { [K in keyof T]: NonNullable<DeepNonNullable<T[K]>> } : T;
